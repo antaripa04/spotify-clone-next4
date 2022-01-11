79c5610b6fadcd4462efbdaa616940d01051ffb6
antaripa04 <antaripapatra99@gmail.com>
2022-01-11 14:16:34 +0530

    initial

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+// import Sidebar from '../components/Sidebar'
+// import Center from '../components/Center'
+// import Player from '../components/Player'
+import { getSession } from 'next-auth/react';
+
+
+export default function Home() {
+  return (
+    // initial div height of screen, for smooth scroll
+    <div className="bg-black h-screen overflow-hidden">
+     <main className="flex text-white">
+        {/* <Sidebar /> */}
+        {/* <Center /> */}
+        Logged in
+     </main>
+
+     <div className="sticky bottom-0">
+        {/* <Player /> */}
+     </div>
+     
+    </div>
+  );
+}
+
+// it will prefetch the session before and then client side will work
+export async function getServerSideProps(context){
+  const session = await getSession(context);
+
+  return {
+    props:{
+      session,
+    },
+  };
+}
