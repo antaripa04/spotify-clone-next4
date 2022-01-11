70dc3e64e9bf9fa408e12337ffb91a582490ebf7
antaripa04 <antaripapatra99@gmail.com>
2022-01-11 14:36:13 +0530

    added in atom

--- atoms/songAtom.js
+++ atoms/songAtom.js
@@ -1,14 +1,18 @@
-import { atom } from 'recoil';
+import { atom } from "recoil";
 
 // which song is clicked
 export const currentTrackIdState = atom({
-    key: "currentTrackIdState",
-    default:null,
+  key: "currentTrackIdState",
+  default: null,
 });
 
 // is it playing or not
 export const isPlayingState = atom({
-    key:"isPlayingState",
-    default: false,
-})
- 
+  key: "isPlayingState",
+  default: false,
+});
+
+export const trackUri = atom({
+  key: "trackUri",
+  default: null,
+});
